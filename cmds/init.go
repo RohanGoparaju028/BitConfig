c3bca83f66c0e6563690db795ff097010b8fbc48
Rohan Goparaju <goparajurohan41@gmail.com>
2026-02-20 11:25:52 -0500

    My init changes

--- cmds/init.go
+++ cmds/init.go
@@ -2,9 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
-const language = map[string][]string {
+var language = map[string][]string {
 	    "typeScript" : []string {"package.json","package-lock.json","yarn.lock"},
 		"javaScript" : []string {"package.json","package-lock.json","yarn.lock"},
 		"python" : []string{"requirments.txt","Pipefile","pyproject.toml"},
@@ -15,13 +16,43 @@ const language = map[string][]string {
 		"rust" :[]string{"cargo.toml"},
 };
 type Initfile struct {
-	Language     []string ` json:"language"`     // For the field  that the devs are coding in
-	Dependencies []string ` json:"dependencies"` // The dependencies that are currently in use or installed
+	Language     []string `json:"language"`     // For the field  that the devs are coding in
+	Dependencies []string `json:"dependencies"` // The dependencies that are currently in use or installed
 	Model        string   `json:"model" `        // LLM that the devolpers are using for the project
 
 }
+func isEmpty(file string) (bool,error) {
+	f,err := os.Open(file)
+	if err != nil {
+		return false,err
+	}
+	defer f.Close()
+	_,err = f.ReadDir(1)
+	if err == io.EOF {
+		return true,nil
+	}
+	if err != nil {
+		return false,err
+	}
+	return false,nil
+}
 func iterateToFindLanguage() []string {
+    f,err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error while getting the current directoy and the error is: ",err)
+		fmt.Println("Exiting by returning an empty string")
+		return []string{}
+	}
+	flag,err := isEmpty(f)
+	if err != nil {
+		fmt.Println("Error while currently checking the current directory",err)
+		return []string{}
+	}
+	if flag {
+		panic("Current directory is empty no project is initialized")
+	} else{
 
+	}
 }
 func DoInit() {
 	fmt.Print("We are initializing the BitConfig in the current directory\nplease choose the appropritate answers that best decribes your project\n")
