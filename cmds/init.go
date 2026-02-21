abeef4ad8bf18ed6067f9b3e9492f71f442ad38b
Rohan Goparaju <goparajurohan41@gmail.com>
2026-02-21 16:38:32 -0500

    MySide changes

--- cmds/init.go
+++ cmds/init.go
@@ -17,10 +17,11 @@ var language = map[string][]string {
 };
 type Initfile struct {
 	Language     []string `json:"language"`     // For the field  that the devs are coding in
-	Dependencies []string `json:"dependencies"` // The dependencies that are currently in use or installed
+	Dependencies map[string]int `json:"dependencies"` // The dependencies that are currently in use or installed
 	Model        string   `json:"model" `        // LLM that the devolpers are using for the project
 
 }
+var dependencyfile = []string{}
 func isEmpty(file string) (bool,error) {
 	f,err := os.Open(file)
 	if err != nil {
@@ -49,15 +50,85 @@ func iterateToFindLanguage() []string {
 		return []string{}
 	}
 	if flag {
-		panic("Current directory is empty no project is initialized")
-	} else{
-
+		fmt.Println("Current directory is empty no project is initialized")
+		return []string {}
 	}
+	dir,err := os.ReadDir(f)
+	if err != nil {
+		fmt.Println(err," cannot read the current directory")
+		return []string{}
+	}
+	languagedetected := []string{}
+	for _,entry := range dir {
+		name  := entry.Name()
+		for lang,dependency := range language {
+			for _,file := range  dependency {
+				if name == file {
+					languagedetected = append(languagdetected,lang)
+					dependencyfile = append(dependencyfile,file)
+				}
+			}
+		}
+	}
+	return languagedetected
+}
+func countLines(files string) (int,err) {
+	file,err := os.Open(files)
+	if err != nil {
+		fmt.Println("Error opening the file",err)
+		return 0,err;
+	}
+	defer file.Close()
+
+  const bufferSize = 4096
+  buffer := make([]byte, bufferSize)
+  lineCount := 0
+  totalBytesRead := 0
+  var lastByte byte
+
+  for {
+    n, err := file.Read(buffer)
+    if n > 0 {
+      totalBytesRead += n
+      lastByte = buffer[n-1]
+
+      for i := 0; i < n; i++ {
+        if buffer[i] == '\n' {
+          lineCount++
+        }
+      }
+    }
+
+    if err == io.EOF {
+      break
+    }
+    if err != nil {
+      return 0, err
+    }
+  }
+
+  if totalBytesRead > 0 && lastByte != '\n' {
+    lineCount++
+  }
+
+  return lineCount, nil
+}
+func TrackDependentFile() map[string]int {
+	dep := map[string]int
+	for _,file := range dependencyfile {
+		lines,err := countLines(file)
+		if err != nil {
+			fmt.Println("Error while processing the file... The error encountered was",err)
+			os.Exit(1)
+		}
+		dep[file] = lines
+	}
+	return dep
 }
 func DoInit() {
 	fmt.Print("We are initializing the BitConfig in the current directory\nplease choose the appropritate answers that best decribes your project\n")
 	var llm string = "Ollama"
-	if _, err := os.Stat(".bitconfig"); err != nil {
+	if _, err := os.Stat(".bitconfig"); err == nil {
 		fmt.Println(".bitconfig already exist in the working  directory")
 		os.Exit(1)
 	} else {
@@ -78,5 +149,6 @@ func DoInit() {
 			os.Exit(1)
 		}
 		language := iterateToFindLanguage()
+		dep := TrackDependen
 	}
 }
